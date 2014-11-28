b1209f8565323f6fbe80cab964f1758df75e4bdd
markuswindegger <markus@mowiso.com>
2014-11-28 21:57:55 +0100

    added directory for de and it
    added directory for de and it, begin of de translations, permissions
    not translated, it only copied from en

---
+++ lang/de/afeatures.php
@@ -0,0 +1,9 @@
+<?php
+$afeatures_list = array(
+	'Ads' => 'Enthält Werbung.',
+	'Tracking' => 'Trackt und sendet deine Aktivitäten irgendwen ohne dein Einverständnis.',
+	'NonFreeNet' => 'Baut auf nicht ersetzbare oder nur sehr schwer ersetzbare Services auf.',
+	'NonFreeAdd' => 'Bewirbt nicht freie Add-Ons, sodass die App eigentlich reine Werbung für eine nicht freie Werbung ist und auch nicht als solche gekennyeichnet worden ist.',
+	'NonFreeDep' => 'Baut auf einer nicht freien Software/Bibliothek auf.'
+);
+?>
---
+++ lang/de/licenses.php
@@ -0,0 +1,15 @@
+<?php
+$licenses_list = array(
+	'Unknown' => "Unbekannt",
+	'GPLv2' => "GNU GPL v2",
+	'GPLv2+' => "GNU GPL v2 oder aktueller",
+	'GPLv3' => "GNU GPL v3",
+	'GPLv3+' => "GNU GPL v3 oder aktueller",
+	'GPL' => "Unbekannte GPL version",
+	'AGPL' => "Afferro GPL Version 3",
+	'Apache2' => "Apache 2",
+	'MIT' => "MIT X11 Lizenz",
+	'BSD' => "Originale BSD Lizenz",
+	'NewBSD' => "Neue BSD Lizenz"
+);
+?>
